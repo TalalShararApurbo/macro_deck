1a4d8f2a0cc5eb138eea37f41a5bc0f5912c26a2
Talal Sharar Apurbo <talal.sharar9611@gmail.com>
2026-04-24 19:02:27 +0600

    Week-2: Glowing Buttons, Toggles, Device Rotation Responsive

--- lib/widgets/macro_button.dart
+++ lib/widgets/macro_button.dart
@@ -1,47 +1,104 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class MacroButton extends StatelessWidget {
+class MacroButton extends StatefulWidget {
   final String label;
   final IconData icon;
+  final bool isToggle;
 
   const MacroButton({
     super.key,
     required this.label,
     required this.icon,
+    this.isToggle = false,
   });
 
   @override
+  State<MacroButton> createState() => _MacroButtonState();
+}
+
+class _MacroButtonState extends State<MacroButton> {
+  bool _isPressed = false;
+  bool _isToggled = false;
+  Timer? _glowLingerTimer;
+
+  bool get _isActive => widget.isToggle ? (_isToggled || _isPressed) : _isPressed;
+
+  @override
+  void dispose() {
+    _glowLingerTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey[900],
-      elevation: 0,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
+    return AnimatedContainer(
+      duration: _isActive ? const Duration(milliseconds: 50) : const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: InkWell(
-        onTap: () {},
-        borderRadius: BorderRadius.circular(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                size: 36.0,
-                color: Colors.white70,
-              ),
-              const SizedBox(height: 12.0),
-              Text(
-                label,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0,
+        boxShadow: _isActive
+            ? [
+                BoxShadow(
+                  color: Colors.lightBlueAccent.withOpacity(0.8),
+                  blurRadius: 16.0,
+                  spreadRadius: 3.0,
                 ),
-              ),
-            ],
+              ]
+            : [],
+      ),
+      child: Card(
+        color: Colors.grey[900],
+        elevation: 0,
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: InkWell(
+          onTap: () {
+            if (widget.isToggle) {
+              setState(() {
+                _isToggled = !_isToggled;
+              });
+            }
+          },
+          onHighlightChanged: (isHighlighted) {
+            _glowLingerTimer?.cancel();
+            if (isHighlighted) {
+              setState(() {
+                _isPressed = true;
+              });
+            } else {
+              _glowLingerTimer = Timer(const Duration(milliseconds: 400), () {
+                if (mounted) {
+                  setState(() {
+                    _isPressed = false;
+                  });
+                }
+              });
+            }
+          },
+          borderRadius: BorderRadius.circular(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.icon,
+                  size: 36.0,
+                  color: _isActive ? Colors.lightBlueAccent : Colors.white70,
+                ),
+                const SizedBox(height: 12.0),
+                Text(
+                  widget.label,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
